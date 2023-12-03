99905416c4e45908b3bd937d44dbe67d76bb2bc3
Salma <ummititli@gmail.com>
2023-12-03 20:06:51 +0600

    scn

--- CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/MagisIssueSerWarSceneController.java
+++ CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/MagisIssueSerWarSceneController.java
@@ -10,8 +10,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MagisIssueSerWarSceneController implements Initializable {
@@ -30,6 +33,20 @@ public class MagisIssueSerWarSceneController implements Initializable {
     private TableColumn<?, ?> badgeNumCol;
     @FXML
     private TableColumn<?, ?> serReasCol;
+    @FXML
+    private TextField warIdTextField;
+    @FXML
+    private TextField instiNameTextField;
+    @FXML
+    private TextField rabOffiIdTextField;
+    @FXML
+    private TextField numOfSubTextField;
+    @FXML
+    private TextField usersNameTextField;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private TextArea viewDraftTextArea;
 
     
     /**
@@ -50,6 +67,14 @@ public class MagisIssueSerWarSceneController implements Initializable {
         Parent secondRoot = loader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(secondRoot));
+    }
+
+    @FXML
+    private void viewDraftButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void issueWarButtonOnClick(ActionEvent event) {
     }
 
 }
