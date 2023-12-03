a13b35e4f8e929c2943d694a437dd98f95bf08c0
Salma <ummititli@gmail.com>
2023-12-03 16:00:49 +0600

    scene switching

--- CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/MagistrateDashSceneController.java
+++ CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/MagistrateDashSceneController.java
@@ -1,10 +1,16 @@
 package mainPkg;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MagistrateDashSceneController implements Initializable {
 
@@ -17,39 +23,75 @@ public class MagistrateDashSceneController implements Initializable {
     }
 
     @FXML
-    private void goToRevCaseDetailsSceneOnClick(ActionEvent event) {
+    private void goToRevCaseDetailsSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("VerdictDetailsInfoScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToViewCaseInfoSceneOnClick(ActionEvent event) {
+    private void goToViewCaseInfoSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("VerdictDetailsInfoScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToSetCourtSchedSceneOnClick(ActionEvent event) {
+    private void goToSetCourtSchedSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("SetCourtScheduleScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToIssueSerWarSceneOnClick(ActionEvent event) {
+    private void goToIssueSerWarSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MagisIssueSerWarScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToVerBarChartSceneOnClick(ActionEvent event) {
+    private void goToContCourtClerkSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ContactCourtClerkScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToContCourtClerkSceneOnClick(ActionEvent event) {
+    private void goToAnnVerCreRepSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AnnounceVerdictScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToAnnVerCreRepSceneOnClick(ActionEvent event) {
+    private void goToAnaCrimActiSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AnalyzCrimActivScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void goToAnaCrimActiSceneOnClick(ActionEvent event) {
+    private void logOutButtonOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(".fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
     @FXML
-    private void logOutButtonOnClick(ActionEvent event) {
+    private void goToVerPieChartSceneOnClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("VerdictDetailsInfoScene.fxml"));
+        Parent secondRoot = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(secondRoot));
     }
 
 }
