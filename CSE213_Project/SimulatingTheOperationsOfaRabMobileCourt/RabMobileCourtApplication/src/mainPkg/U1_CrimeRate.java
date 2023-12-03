9b06eee48324998f249eb414dfc453fdc87d5a68
Aaanika-Islam <nusratanika214@gmail.com>
2023-12-03 19:40:16 +0600

    CR

--- CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/U1_CrimeRate.java
+++ CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/U1_CrimeRate.java
@@ -9,6 +9,30 @@ package mainPkg;
  * @author Anika
  */
 public class U1_CrimeRate {
+    private int crimeCount;
+    private String districtName;
+
+    public U1_CrimeRate(int crimeCount, String districtName) {
+        this.crimeCount = crimeCount;
+        this.districtName = districtName;
+    }
+
+    public int getCrimeCount() {
+        return crimeCount;
+    }
+
+    public void setCrimeCount(int crimeCount) {
+        this.crimeCount = crimeCount;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+    
     
     
 }
