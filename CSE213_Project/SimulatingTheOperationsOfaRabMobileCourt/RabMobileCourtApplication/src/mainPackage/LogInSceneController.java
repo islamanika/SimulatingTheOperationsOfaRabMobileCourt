7fbde2e80f5dba7ac007f7021105d914bb1f0d35
Salma <ummititli@gmail.com>
2023-12-03 23:33:44 +0600

    Update LogInSceneController.java

--- CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPackage/LogInSceneController.java
+++ CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPackage/LogInSceneController.java
@@ -4,20 +4,23 @@
  */
 package mainPackage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author tabri
- */
+
 public class LogInSceneController implements Initializable {
 
     @FXML
@@ -36,9 +39,6 @@ public class LogInSceneController implements Initializable {
     }    
 
     @FXML
-<<<<<<< Updated upstream
-    private void logInButtonClick(ActionEvent event) {
-=======
     private void logInButtonClick(ActionEvent event) throws IOException {
         int userId;
         String pass_str;
@@ -139,7 +139,6 @@ public class LogInSceneController implements Initializable {
             a2.setContentText("Wrong ID/Password. Please Try Again");
             a2.showAndWait();
         }
->>>>>>> Stashed changes
     }
 
     @FXML
