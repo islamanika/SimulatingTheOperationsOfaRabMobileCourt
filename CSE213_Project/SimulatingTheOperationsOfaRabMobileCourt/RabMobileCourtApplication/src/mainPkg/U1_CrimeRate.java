1a949446751794235a65323a75a1498b2d305ede
Aaanika-Islam <nusratanika214@gmail.com>
2023-12-03 19:38:00 +0600

    CR

---
+++ CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/U1_CrimeRate.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainPkg;
+
+/**
+ *
+ * @author Anika
+ */
+public class U1_CrimeRate {
+    
+    
+}
