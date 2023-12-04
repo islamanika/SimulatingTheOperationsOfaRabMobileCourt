4a6dfcc85429609256e60530ae0911a735a3d409
Salma <ummititli@gmail.com>
2023-12-04 22:31:28 +0600

    solid

--- CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/ContactCourtClerkSceneController.java
+++ CSE213_Project/SimulatingTheOperationsOfaRabMobileCourt/RabMobileCourtApplication/src/mainPkg/ContactCourtClerkSceneController.java
@@ -55,7 +55,7 @@ public class ContactCourtClerkSceneController implements Initializable {
 
     @FXML
     private void returnHomeButtonOnClick(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("RabOfficerDashScene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MagistrateDashScene.fxml"));
         Parent secondRoot = loader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(secondRoot));
